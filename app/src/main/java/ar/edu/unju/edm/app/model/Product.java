ab085d18a3b6d7ec5a30758945a5ac1bc087e535
Meedrem <jcgmeedrem@gmail.com>
2023-05-02 12:06:48 -0300

    Corrección del Modelo 🍃

--- app/src/main/java/ar/edu/unju/edm/app/model/Product.java
+++ app/src/main/java/ar/edu/unju/edm/app/model/Product.java
@@ -2,61 +2,83 @@ package ar.edu.unju.edm.app.model;
 
 import org.springframework.stereotype.Component;
 
-@component
-public class Product  {
-	
-	private int ID;
-	private String nombre;
-	private String descripcion;
-	private String precio;
-	private String marca;
-	private int Stock;
-	private String URL;
-	
-	public Product () {}
-	
-	public Product (int ID,String nombre, String descripcion, String precio, String marca, int Stock,String URL) {}
+@Component
+public class Product {
+
+    private int ID;
+    private String nombre;
+    private String descripcion;
+    private String precio;
+    private String marca;
+    private int stock;
+    private String URL;
+
+    public Product() {}
+
+    public Product(int ID, String nombre, String descripcion, String precio, String marca, int stock, String URL) {
         this.ID = ID;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
         this.marca = marca;
-        this.Stock = stock;
-        this.URL = URL
+        this.stock = stock;
+        this.URL = URL;
     }
-    public String getNombre () {
-    	return nombre;
+
+    public int getID() {
+        return ID;
     }
-    public void setNombre (String nombre) {
+
+    public void setID(int iD) {
+        ID = iD;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
-}
-    public String getDescripcion () {
+    }
+
+    public String getDescripcion() {
         return descripcion;
     }
-    public void setDescripcion (String descripcion) {
-    	this.descripcion = descripcion;
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
-    public String getPrecio () {
-    	return precio;
+
+    public String getPrecio() {
+        return precio;
     }
-    public void setPrecio (String precio) {
-    	this.precio = precio;
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
     }
-    public String getMarca () {
-    	return marca;
+
+    public String getMarca() {
+        return marca;
     }
-    public void setMarca (String marca) {
-    	this.marca = marca;
+
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
-    public int getID () {
-    	return ID;
+
+    public int getStock() {
+        return stock;
     }
-    public void setStock (int Stock) {
-    	this.Stock = Stock;
+
+    public void setStock(int stock) {
+        this.stock = stock;
     }
-    public String getURL () {
-    	return URL;
+
+    public String getURL() {
+        return URL;
     }
-    public void setURL (String URL) {
-    	this.URL = URL
+
+    public void setURL(String uRL) {
+        URL = uRL;
     }
+
+}
